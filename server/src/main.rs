5d0bb9f962eb4395c6f143976d43fd1890bf7bec
Sven65 <Sven65@users.noreply.github.com>
2024-03-08 11:39:51 +0100

    Start project

---
+++ server/src/main.rs
@@ -0,0 +1,78 @@
+mod state;
+
+use axum::routing::get;
+use socketioxide::{
+    extract::{Data, SocketRef, State},
+    SocketIo,
+};
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
+#[derive(Debug, serde::Deserialize)]
+struct MessageIn {
+    room: String,
+    text: String,
+}
+
+#[derive(serde::Serialize)]
+struct Messages {
+    messages: Vec<state::Message>,
+}
+
+
+async fn on_connect(socket: SocketRef) {
+    info!("socket connected: {}", socket.id);
+
+    socket.on(
+        "join",
+        |socket: SocketRef, Data::<String>(room)| async move {
+            info!("Received join {:?}", room);
+            let _ = socket.leave_all();
+            let _ = socket.join(room.clone());
+            // let messages = store.get(&room).await;
+            // let _ = socket.emit("messages", Messages { messages });
+        },
+    );
+
+    socket.on(
+        "hello",
+        |socket: SocketRef| async move {
+            info!("Got hello");
+            socket.emit("hello", "to you too").unwrap();
+        },
+    );
+}
+
+async fn handler(axum::extract::State(io): axum::extract::State<SocketIo>) {
+    info!("handler called");
+    let _ = io.emit("hello", "world");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let (layer, io) = SocketIo::builder().build_layer();
+
+    io.ns("/", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello World" }))
+        .route("/hello", get(handler))
+        .with_state(io)
+        .layer(
+            ServiceBuilder::new()
+            .layer(CorsLayer::permissive())
+            .layer(layer)
+        );
+
+    info!("Starting server on port {}", "3000");
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
