1057df913b36dbf26eafe6221e8c65a5509c9b18
Sven65 <Sven65@users.noreply.github.com>
2024-03-08 11:52:24 +0100

    Make eslint work

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import { render } from 'preact';
-import { LocationProvider, Router, Route } from 'preact-iso';
+import { render } from 'preact'
+import { LocationProvider, Router, Route } from 'preact-iso'
 
-import { Header } from './components/Header.jsx';
-import { Home } from './pages/Home/index.jsx';
-import { NotFound } from './pages/_404.jsx';
-import './style.css';
+import { Header } from './components/Header.jsx'
+import { Home } from './pages/Home/index.jsx'
+import { NotFound } from './pages/_404.jsx'
+import './style.css'
 
 export function App() {
 	return (
@@ -17,7 +17,7 @@ export function App() {
 				</Router>
 			</main>
 		</LocationProvider>
-	);
+	)
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
