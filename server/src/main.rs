ee3d82bbbcee80a514be66b566ce5de40ed6f14b
Sven65 <Sven65@users.noreply.github.com>
2024-03-22 14:39:29 +0100

    Dockerize this shit

--- server/src/main.rs
+++ server/src/main.rs
@@ -401,7 +401,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("Starting server on port {}", "3000");
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await?;
 
