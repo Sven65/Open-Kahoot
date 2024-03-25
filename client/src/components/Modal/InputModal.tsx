0ec6358d15ac473b16e19258a9e6ca692b6eb32b
Sven65 <Sven65@users.noreply.github.com>
2024-03-25 15:12:16 +0100

    Add input modal for creating new quiz

---
+++ client/src/components/Modal/InputModal.tsx
@@ -0,0 +1,63 @@
+import { ComponentChildren } from 'preact'
+
+import './Modal.scss'
+import { Input } from '../Form/Input'
+import { useState } from 'preact/hooks'
+
+type Props =  {
+	show: boolean,
+	title?: string,
+	onClose?: () => void,
+	onAction?: (value: string) => void,
+	children?: ComponentChildren,
+	text?: string
+	actionText?: string
+}
+
+export const InputModal = ({
+	show = false,
+	title,
+	text,
+	actionText,
+	onAction,
+	onClose,
+}: Props) => {
+	const [ value, setValue ] = useState('')
+	if (!show) return null
+
+	return (
+		<div class="relative z-20">
+			<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+			<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+				<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+					<div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+						<div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+							<div class="sm:flex sm:items-start">
+								<div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+									<svg class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+										<path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+									</svg>
+								</div>
+								<div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+									<h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">{title}</h3>
+									<div class="mt-2">
+										<p class="text-sm text-gray-500">{text}</p>
+										<Input
+											placeholder={'My Awesome Quiz'}
+											value={value}
+											onChange={e => setValue(e.target.value)}
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+							<button type="button" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto" onClick={() => onAction(value)}>{actionText}</button>
+							<button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={onClose}>Cancel</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- client/src/pages/Me/index.tsx
+++ client/src/pages/Me/index.tsx
@@ -5,8 +5,8 @@ import { useLocation } from 'preact-iso'
 import { Button } from '../../components/Form/Button'
 import { GameContext } from '../../context/GameContext'
 import { Input } from '../../components/Form/Input'
-import { Modal } from '../../components/Modal/Modal'
 import { DashboardLayout } from '../../components/Layouts/Dashboard/Dashboard'
+import { InputModal } from '../../components/Modal/InputModal'
 
 const InternalMe = () => {
 	const apiContext = useContext(ApiContext)
@@ -28,17 +28,25 @@ const InternalMe = () => {
 
 	return (
 		<DashboardLayout>
+			<InputModal
+				show={showModal}
+				onClose={() => setShowModal(false)}
+				title='Enter quiz name'
+				actionText='Create!'
+				onAction={apiContext.createQuiz}
+			/>
+
 			<h1>Welcome, {apiContext.user.username}</h1>
 
 			<h1>Quizzes:</h1>
 			{apiContext.userQuizzes.map((quiz) => (
 				<div key={quiz.id}>
 					{quiz.name}
-					<Button onClick={() => location.route(`/quiz/${quiz.id}/edit`)}>Edit</Button>
-					<Button onClick={() => { gameContext.createRoom(quiz.id) }} color="green">Host</Button>
+					<Button onClick={() => location.route(`/quiz/${quiz.id}/edit`)} bgColor="green-500">Edit</Button>
+					<Button onClick={() => { gameContext.createRoom(quiz.id) }} bgColor="green-500">Host</Button>
 				</div>
 			))}
-			<Button color="green" onClick={() => setShowModal(true)}>Create new quiz</Button>
+			<Button bgColor="green-500" onClick={() => setShowModal(true)}>Create new quiz</Button>
 		</DashboardLayout>
 	)
 }
