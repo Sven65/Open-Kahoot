02656ac67d8e9fe5842a09ab196ffba204afe3aa
Sven65 <Sven65@users.noreply.github.com>
2024-03-28 17:04:38 +0100

    Add s3 upload

--- server/src/api/files/s3_storage.rs
+++ server/src/api/files/s3_storage.rs
@@ -1,38 +1,26 @@
 use async_trait::async_trait;
-use aws_sdk_s3::Client;
-use tracing::info;
-use std::net::ToSocketAddrs;
+use rusoto_core::Region;
+use rusoto_s3::{PutObjectRequest, S3Client, S3};
 
 use super::file_storage_engine::FileStorageEngine;
-use std::env;
+use std::{env, str::FromStr};
 
 #[derive(Clone)]
 pub struct S3Storage {
-	client: Client,
+	client: S3Client,
 }
 
 impl S3Storage {
 	pub async fn new() -> Self {
-		let aws_configuration = aws_config::load_from_env().await;
-		
-		let aws_configuration = match env::var("S3_ENDPOINT") {
-			Ok(endpoint) => {
-				println!("endpoint is {}", endpoint);
-				aws_configuration
-				.to_builder()
-				.endpoint_url(endpoint)
-				.build()
-			}
-			Err(_) => aws_configuration,
-		};
-		
-		info!("endpoint url {:#?}", aws_configuration.endpoint_url());
+		let region = Region::from_str(&env::var("AWS_REGION").expect("AWS_REGION must be set.")).unwrap();
 
-		//create aws s3 client
-		let client = Client::new(&aws_configuration);
+		let client = match env::var("S3_ENDPOINT") {
+			Ok(endpoint) => S3Client::new(Region::Custom { name: "custom-ep".to_string(), endpoint }),
+			Err(_) => S3Client::new(region)
+		};
 
 		Self {
-			client,
+			client
 		}
 	}
 }
@@ -41,18 +29,19 @@ impl S3Storage {
 #[async_trait]
 impl FileStorageEngine for S3Storage {
 	async fn upload_file(&self, data: &[u8], file_name: String) -> Result<String, std::io::Error> {		
-		let res = self.client.put_object()
-			.key(file_name)
-			.bucket(env::var("AWS_S3_BUCKET").expect("Expected S3 bucket to be set."))
-			.body(data.to_vec().into())
-			.send()
-			.await;
-
-
-		if res.is_err() {
-			println!("Failed to upload {:#?}", res.err());
+		let request = PutObjectRequest {
+			bucket: env::var("S3_BUCKET").expect("S3 bucket must be set.").to_string(),
+			key: file_name.clone(),
+			body: Some(data.to_vec().into()), // Convert the byte array into a readable stream
+			..Default::default()
+		};
+	
+		// Upload the file to S3
+		match self.client.put_object(request).await {
+			Ok(_) => println!("File uploaded successfully"),
+			Err(err) => eprintln!("Error uploading file: {}", err),
 		}
-		
-		Ok("s3 engine".to_string())
+
+		Ok(file_name)
 	}
 }
