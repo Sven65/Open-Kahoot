ed2db07a4d5a85c3afe251ddb2b8bd9aa4f3df08
Sven65 <Sven65@users.noreply.github.com>
2024-04-03 10:24:01 +0200

    Make email verification work on server

--- server/src/api/mod.rs
+++ server/src/api/mod.rs
@@ -4,36 +4,24 @@ pub mod quiz;
 pub mod question;
 pub mod quiz_types;
 pub mod files;
+pub mod email;
 
 use std::sync::Arc;
 
-use axum::{routing::{get, post}, Router};
-use tracing::info;
+use axum::{routing::get, Router};
 
-use crate::{email::Email, AppState};
+use crate::AppState;
 
-use self::{files::files_router, question::question_router, quiz::quiz_router, user::user_router};
+use self::{email::email_router, files::files_router, question::question_router, quiz::quiz_router, user::user_router};
 
 async fn root() -> &'static str {
 	"Hello world"
 }
 
-async fn send_email() -> &'static str {
-	let mail = Email::new().unwrap();
-
-	let result = mail.send("Test Email", "thormax5@gmail.com", "Hello, this is a test email").await;
-
-	if result.is_err() {
-		info!("Error sending email {:#?}", result.err());
-	}
-
-	"Email sent"
-}
-
 pub fn api_router(state: Arc<AppState>) -> Router {
 	Router::new()
 		.route("/", get(root))
-		.route("/email", post(send_email))
+		.nest("/email", email_router(Arc::clone(&state)))
 		.nest("/user", user_router(Arc::clone(&state)))
 		.nest("/quiz", quiz_router(Arc::clone(&state)))
 		.nest("/question", question_router(Arc::clone(&state)))
