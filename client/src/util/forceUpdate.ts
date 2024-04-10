ba3833b4ce702c55809b0d16a73a0c62843312c2
Sven65 <Sven65@users.noreply.github.com>
2024-04-10 21:58:07 +0200

    Remove unneeded code

--- client/src/util/forceUpdate.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'preact/hooks'
-
-export const useForceUpdate = () => {
-	const [ value, setValue ] = useState(0) // integer state
-	return () => setValue(value => value + 1) // update state to force render
-	// A function that increment 👆🏻 the previous state like here 
-	// is better than directly setting `setValue(value + 1)`
-}
